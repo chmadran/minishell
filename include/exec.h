ec6f673a0144031a16938336b19b9254f0cd9bef
Charlotte MADRANGEAS <charlotte.madrangeas@gmail.com>
2023-07-24 11:26:02 +0200

    (echo.c) add echo builtins

--- include/exec.h
+++ include/exec.h
@@ -6,7 +6,7 @@
 /*   By: chmadran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 11:06:41 by chmadran          #+#    #+#             */
-/*   Updated: 2023/07/24 11:06:43 by chmadran         ###   ########.fr       */
+/*   Updated: 2023/07/24 11:16:20 by chmadran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 # include "minishell.h"
 
 extern t_master	g_master;
+
+/* builtins */
+
+int	ft_echo(int argc, char **argv);
 
 
 #endif
--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,13 +6,36 @@
 /*   By: chmadran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 11:03:13 by chmadran          #+#    #+#             */
-/*   Updated: 2023/07/24 11:03:33 by chmadran         ###   ########.fr       */
+/*   Updated: 2023/07/24 11:25:31 by chmadran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "minishell.h"
 #include "libft.h"
 #include "exit.h"
 #include "env.h"
 #include "exec.h"
+
+int	ft_echo(int argc, char **argv)
+{
+	int		i;
+	bool	is_option;
+
+	i = 0;
+	is_option = false;
+	if (argc > 1 && !ft_strncmp(argv[1], "-n", 2))
+	{
+		i = 1;
+		is_option = true;
+	}
+	while (argv[i])
+	{
+		ft_putstr_fd(argv[i], 1);
+		if (argv[i + 1])
+			ft_putstr_fd(" ", 1);
+		i++;
+	}
+	if (!is_option)
+		ft_putstr_fd("\n", 1);
+	return (EXIT_SUCCESS);
+}
