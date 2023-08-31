23da07ad45ef53d806b2a311eac233b353ac002f
Charlotte MADRANGEAS <charlotte.madrangeas@gmail.com>
2023-08-31 15:11:59 +0200

    HANDLE LIMITER EXPANSION HEREDOC

--- src/exec_prep/heredoc_expansion.c
+++ src/exec_prep/heredoc_expansion.c
@@ -6,7 +6,7 @@
 /*   By: chmadran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 18:53:07 by chmadran          #+#    #+#             */
-/*   Updated: 2023/08/31 14:23:04 by chmadran         ###   ########.fr       */
+/*   Updated: 2023/08/31 15:11:34 by chmadran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,8 @@ static char	*process_expansion_replace_heredoc(char *substr_start,
 	value = NULL;
 	final_str = NULL;
 	heredoc_limiter = false;
-	(void)limiter;
+	if (ft_strcmp(new_str, limiter) == 0)
+		heredoc_limiter = true;
 	if (is_valid_name(substr_start) == EXIT_FAILURE)
 		return (new_str);
 	name = extract_expansion_name(substr_start);
@@ -96,7 +97,7 @@ char	*search_expansion_heredoc(char *arg, char *limiter)
 	new_str = ft_strdup(arg);
 	while (new_str[i])
 	{
-		if (new_str[i] == '$' && !inside_single_quotes(new_str, i))
+		if (new_str[i] == '$' && !inside_single_quotes(new_str, i) && new_str[i + 1])
 		{
 			new_str = process_expansion_replace_heredoc(&new_str[i],
 					new_str, i, limiter);
